8b2802a722719028109f99dce2924a3b506c074c
Prashant Kumar Yadav <kartik224blue@gmail.com>
2022-12-18 14:04:15 +0530

    update mongodb atlas connection link

--- src/database/index.js
+++ src/database/index.js
@@ -4,7 +4,8 @@ let blockChainModel=require("./model");
 
 
 const connectToMongo=(callback)=>{
-    mongoose.connect(process.env.MONGODB_URI||"mongodb+srv://code_hobby:code_hobby@cluster0.1ludt.mongodb.net/blockChain?retryWrites=true&w=majority",(err)=>{
+    code='mongodb+srv://code_hobby:code_hobby@cluster0.1ludt.mongodb.net/?retryWrites=true&w=majority'
+    mongoose.connect(process.env.MONGODB_URI||code,(err)=>{
     if(err) return console.log("connot connect to db",err.message);
     console.log("DB is connected");
     callback();
